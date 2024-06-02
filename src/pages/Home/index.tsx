82394409dec40cd2436c3549fa563faace106264
Juan Carlos Padillo <juancarlospadillo23@gmail.com>
2024-06-02 23:01:54 +0800

    Added hero section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,10 @@
-import React, { useState } from "react";
+import Hero from "../../components/Home/Hero";
 
 const Home = () => {
-  const [count, setCount] = useState(0);
-  const handleCount = () => setCount(count + 1);
   return (
-    <div>
-      <h1>Count: {count}</h1>
-      <button className="bg-red-300 p-2" onClick={handleCount}>
-        Increment
-      </button>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 };
 
